11ea7a7836f0a6bfdb52e4a9678cb1650a27cca6
paulinapyrc <36887279+paulinapyrc@users.noreply.github.com>
2018-06-04 00:02:46 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,21 @@
+import random
+plansza = [1,2,3,
+           4,5,6,
+           7,8,9]
+def NarysujPole:
+    print ('  |  |')
+    print (' '+board[1]+ ' | ' + board[2] + ' | ' +board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print (' '+board[4]+ ' | ' + board[5] + ' | ' +board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print (' '+board[7]+ ' | ' + board[8] + ' | ' +board[9])
+    print('   |   |')      
+        
+        
 
+        
         
