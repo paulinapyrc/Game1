37b685a07c45e6a0606fc278534b95e5813bb510
sylwiak2777 <36968897+sylwiak2777@users.noreply.github.com>
2018-06-04 08:00:20 +0200

    nawiasy, zamiana board

--- tictactoe.py
+++ tictactoe.py
@@ -4,7 +4,7 @@ plansza = [1,2,3,
            7,8,9]
 def NarysujPole():
     print ('  |  |')
-    print (' '+board[1]+ ' | ' + board[2] + ' | ' +board[3])
+    print (' '+board[1]+ ' | ' + board[2] + ' | ' +board[3])   #WYDAJE MI SIĘ, ŻE ZAMIAST ' | ' POWINNO BYĆ WSZĘDZIE ('|'), JAK MYŚLICIE?
     print('   |   |')
     print('-----------')
     print('   |   |')
@@ -19,11 +19,16 @@ while True:
            input = input("Wybierz pole: ")
            input = int(input)
            
-           if plansza[input] != 'X' and plansza[input] !='O':
-                      plansza[input] = 'X'
+           if board[input] != 'X' and board[input] !='O':  #POZMIENIAŁAM 'PLANSZA' NA 'BOARD', BO WYŻEJ W KOLUMNACH SĄ BOARDY I TRZEBA SIĘ TRZYMAĆ TEJ NAZWY. I GUESS
+                      board[input] = 'X'
                       random.seed() # Randomowe liczby
+                      opponent = random.randint(0,8)
+                      
+                      if board(opponent) != "o" and board (opponent) !="x":
+                                 board(opponent) = "o"
            else:
-                      print "To miejsce jest już zajęte"
+                      print ("To miejsce jest już zajęte. Wybierz inne")
+                      
      
          
 
