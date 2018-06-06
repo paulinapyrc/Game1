dc0555f9abc40d1bedfc78824fe1c03781da0d8b
paulinapyrc <36887279+paulinapyrc@users.noreply.github.com>
2018-06-06 17:40:09 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,14 @@
 import random
 plansza = [0,1,2,
-          3,4,5,
-          6,7,8]
+           3,4,5,
+           6,7,8]
 def NarysujPole():
-    print ('  |  |')
-    print (' '+board[0]+ ' | ' + board[1] + ' | ' +board[2])   #WYDAJE MI SIĘ, ŻE ZAMIAST ' | ' POWINNO BYĆ WSZĘDZIE ('|'), JAK MYŚLICIE?
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print (' '+board[3]+ ' | ' + board[4] + ' | ' +board[5])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print (' '+board[6]+ ' | ' + board[7] + ' | ' +board[8])
-    print('   |   |')      
+    print (board[0], '|', board[1], '|', board[2])
+    print ('---------')
+    print (board[3], '|', board[4], '|', board[5])
+    print ('---------')
+    print (board[6], '|', board[7], '|', board[8])
+    print ('---------')      
 
 def Sprawdz(char, spot1, spot2, spot3):
     if board[spot1] == char and board[spot2] == char and board[spot3] == char:
